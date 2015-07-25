cbba155ee994ff116f319e520db6b93d0f2a51e3
techninja <james@tn42.com>
2015-07-25 01:48:48 -0700

    Revamp pre-build to work everywhere Fingers crossed!

--- tasks/pre-build-task.js
+++ tasks/pre-build-task.js
@@ -6,6 +6,10 @@ module.exports = function(grunt) {
   var log = grunt.log.writeln;
   var conf = grunt.config;
   var fs = require('./task-helpers')(grunt);
+  var unzip = require('unzip2');
+  var request = require('request');
+  var fsp = require('fs-plus');
+  var jsonfile = require('jsonfile');
 
   var branch = conf('rpbuild.branch');
   var ageIgnore = 1000 * 60 * 60; // 1000 milliseconds, 60 seconds, 60 minutes (one hour)
@@ -33,27 +37,44 @@ module.exports = function(grunt) {
     fs.mkdir('out/prebuild');
 
     log('Extracting and preparing latest RoboPaint from branch: ' + branch);
-      log(fs.run('curl ' + conf('rpbuild.repo').replace('github', 'codeload.github') + '/zip/' + branch + ' -so out/prebuild/robopaint.zip'));
-      log(fs.run('unzip -q out/prebuild/robopaint.zip -d out/prebuild'));
-      fs.rm('out/prebuild/robopaint.zip');
-      fs.mv('out/prebuild/robopaint-' + branch, 'out/prebuild/robopaint');
-      log(fs.run('sed -i -e \'s/"stage": "development"/"stage": "release"/g\' out/prebuild/robopaint/package.json'));
-    log('Installing RoboPaint dependencies...');
-      log(fs.run('cd out/prebuild/robopaint/ && npm install --production --silent'));
-    log("GO on a diet! Removing extra fat from build...");
-      var path = 'out/prebuild/robopaint/';
-      fs.rm(path + 'resources/modes/edit/method-editor/build/');
-      fs.rm(path + 'resources/modes/edit/method-editor/docs/');
-      fs.rm(path + 'resources/modes/edit/method-editor/method-draw/');
-      fs.rm(path + 'resources/modes/edit/method-editor/test/');
-      fs.rm(path + 'node_modules/cncserver/node_modules/serialport/build/Release');
-      fs.rm(path + 'node_modules/cncserver/node_modules/serialport/build/serialport');
-    log("Copying native build components for Windows, Linux & OSX...");
-      fs.cp('native_builds/serialport/', 'out/prebuild/robopaint/node_modules/cncserver/node_modules/serialport/build/serialport/');
-    log("Flattening node modules path structure...");
-    flatten('out/prebuild/robopaint', {}, function(){
-      log('Prebuild complete!');
-      done();
-    });
+    var zipSrc = conf('rpbuild.repo').replace('github', 'codeload.github') + '/zip/' + branch;
+    var zipDest = 'out/prebuild/robopaint.zip';
+
+    request(zipSrc)
+      .pipe(fsp.createWriteStream(zipDest))
+      .on('close', function() {
+        fsp.createReadStream(zipDest)
+          .pipe(unzip.Extract({path: 'out/prebuild'})).on('close', function(){
+            fs.rm(zipDest);
+            fs.mv('out/prebuild/robopaint-' + branch, 'out/prebuild/robopaint');
+
+            // Change out the package stage environment to release
+            var file = 'out/prebuild/robopaint/package.json';
+            jsonfile.writeFileSync(file, jsonfile.readFileSync(file, {
+              reviver: function(k, v) {
+                if (k === 'stage' && v === 'development') {return 'release'}
+                return v;
+              }
+            }), {spaces: 2});
+
+            log('Installing RoboPaint dependencies...');
+              log(fs.run('cd out/prebuild/robopaint/ && npm install --production --silent'));
+            log("GO on a diet! Removing extra fat from build...");
+              var path = 'out/prebuild/robopaint/';
+              fs.rm(path + 'resources/modes/edit/method-editor/build/');
+              fs.rm(path + 'resources/modes/edit/method-editor/docs/');
+              fs.rm(path + 'resources/modes/edit/method-editor/method-draw/');
+              fs.rm(path + 'resources/modes/edit/method-editor/test/');
+              fs.rm(path + 'node_modules/cncserver/node_modules/serialport/build/Release');
+              fs.rm(path + 'node_modules/cncserver/node_modules/serialport/build/serialport');
+            log("Copying native build components for Windows, Linux & OSX...");
+              fs.cp('native_builds/serialport/', 'out/prebuild/robopaint/node_modules/cncserver/node_modules/serialport/build/serialport/');
+            log("Flattening node modules path structure...");
+            flatten('out/prebuild/robopaint', {}, function(){
+              log('Prebuild complete!');
+              done();
+            });
+        });
+      });
   });
 };
